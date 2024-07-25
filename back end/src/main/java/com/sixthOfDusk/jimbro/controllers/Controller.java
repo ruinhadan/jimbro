c0816d82e03f2c972206e1fb63135c036b7072f6
ruinhadan <ihateyoutheory@live.com>
2024-07-25 22:54:15 +0530

    Updated directory structure: 1. Created separate folders for front end and back end. 2. Added boilerplate code for the back end.

---
+++ back end/src/main/java/com/sixthOfDusk/jimbro/controllers/Controller.java
@@ -0,0 +1,12 @@
+package com.sixthOfDusk.jimbro.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/hello")
+    public String getHome() {
+        return "Hello";
+    }
+}
---
+++ back end/src/test/java/com/sixthOfDusk/jimbro/JimbroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixthOfDusk.jimbro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimbroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
